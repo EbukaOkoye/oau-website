81336a12a2dd44a05e96ff96bcf1a1a036395253
Ifenna <ifenna@Ifennas-MacBook-Pro.local>
2025-06-28 00:44:36 +0100

    everything good here

--- src/utils/icons.ts
+++ src/utils/icons.ts
@@ -20,10 +20,12 @@ import {
 } from "react-icons/fa";
 import { FaLocationDot } from "react-icons/fa6";
 import { MdEmail } from "react-icons/md";
-import { IoTimeSharp } from "react-icons/io5";
+import { IoTimeSharp, IoCloseSharp } from "react-icons/io5";
 import { IoIosSend } from "react-icons/io";
 import { GiReceiveMoney } from "react-icons/gi";
 import { BsChatDotsFill } from "react-icons/bs";
+import { RiMenu2Fill } from "react-icons/ri";
+
 
 export const Phone = FaPhoneAlt;
 export const CircleArrow = FaArrowCircleRight;
@@ -47,6 +49,78 @@ export const CalendarCheck = FaCalendarCheck;
 export const Email = MdEmail;
 export const Send = IoIosSend;
 export const Time = IoTimeSharp;
+export const CloseX = IoCloseSharp;
 export const ReceiveMoney = GiReceiveMoney;
 export const Chat = BsChatDotsFill;
+export const Menu = RiMenu2Fill;
 
+
+
+
+// open={open}
+          // onClose={closeDrawer}
+          // className={`p-4 bg-white z-[1000] translate-x-full ${
+          //   !open ? "!-translate-x-full" : "!translate-x-[1000%]"
+          // } lg:hidden `}
+          // placement="left"
+          // size={300}
+
+
+
+      //     <Nav className="bg-gradient-to-r from-main-purple to-main-pink rounded-none border-0">
+      //   <div className="flex justify-between px-4 py-3 md:px-8">
+      //     <div className="hidden lg:flex w-full lg:justify-between gap-6 text-sm font-semibold">
+      //       {navbarLinks.map((_link) => (
+      //         <Link
+      //           key={_link.name}
+      //           href={_link.link}
+      //           className={`hover:bg-white hover:text-main-purple p-3 ${
+      //             _link.link === pathname
+      //               ? "bg-white text-main-purple"
+      //               : "text-white"
+      //           }`}
+      //         >
+      //           {_link.name}
+      //         </Link>
+      //       ))}
+      //     </div>
+
+      //     <div className="lg:hidden mb-3">
+      //       <IconButton
+      //         variant="text"
+      //         // onClick={() => setOpen(true)}
+      //         onClick={() => setOpenNav(!openNav)}
+      //         className="mb-3"
+      //       >
+      //         <Menu className="h-6 w-6 text-white my-auto mb-3" />
+      //       </IconButton>
+      //     </div>
+      //   </div>
+
+      //   <Collapse open={openNav}>
+      //     <div className="flex justify-between items-center mb-4">
+      //       <span className="font-bold text-lg text-deep-orange">Menu</span>
+      //       {/* <IconButton variant="text" onClick={closeDrawer}> */}
+      //       <CloseX
+      //         className="h-6 w-6 text-deep-orange cursor-pointer"
+      //         // onClick={closeDrawer}
+      //         onClick={() => setOpenNav(!openNav)}
+      //       />
+      //       {/* </IconButton> */}
+      //     </div>
+      //     <div className="flex flex-col gap-4">
+      //       {navbarLinks.map((_link) => (
+      //         <a
+      //           key={_link.name}
+      //           href={_link.link}
+      //           onClick={() => setOpenNav(!openNav)}
+      //           className={`text-main-pink hover:text-main-purple ${
+      //             _link.link === pathname && "!text-main-pink"
+      //           } text-sm font-medium`}
+      //         >
+      //           {_link.name}
+      //         </a>
+      //       ))}
+      //     </div>
+      //   </Collapse>
+      // </Nav>
