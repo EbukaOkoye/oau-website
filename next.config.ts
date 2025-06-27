984bc0e49bdbdd8033c64de3cf1e8e5096713106
Ifenna <ifenna@Ifennas-MacBook-Pro.local>
2025-06-27 12:56:44 +0100

    config output

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export", // Best for Render/Node.js hosting
 };
 
 export default nextConfig;
