ea47125052244fe51a650d5a3819a83b3ed99f0b
Ifenna <ifenna@Ifennas-MacBook-Pro.local>
2025-12-12 10:01:52 +0100

    another push again

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,19 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+// import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 // import Navbar from "@/components/Navbar";
 import "aos/dist/aos.css";
-import AOSProvider from "@/providers/AosProvider";
+// import AOSProvider from "@/providers/AosProvider";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"],
+// });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
 
 export const metadata: Metadata = {
   title: "The OAU FOundation",
@@ -22,21 +22,32 @@ export const metadata: Metadata = {
   },
 };
 
-export default function RootLayout({}: // children,
-Readonly<{
-  children: React.ReactNode;
-}>) {
-  return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased scroll-smooth`}
-      >
-        <section className="xl:container lg:mx-auto">
-          {/* <Navbar /> */}
-          <AOSProvider />
-          {/* {children} */}
-        </section>
-      </body>
-    </html>
-  );
+export default function RootLayout() {
+  <html lang="en">
+    <body>
+      <section>
+        <h2 className="">Hello</h2>
+      </section>
+    </body>
+  </html>;
 }
+
+// export default function RootLayout({
+//   children,
+// }: Readonly<{
+//   children: React.ReactNode;
+// }>) {
+//   return (
+//     <html lang="en">
+//       <body
+//         className={`${geistSans.variable} ${geistMono.variable} antialiased scroll-smooth`}
+//       >
+//         <section className="xl:container lg:mx-auto">
+//           {/* <Navbar /> */}
+//           <AOSProvider />
+//           {children}
+//         </section>
+//       </body>
+//     </html>
+//   );
+// }
